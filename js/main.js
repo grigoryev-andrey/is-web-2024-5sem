067054c372f8a46ace9357d4281245f698634151
Your Name <you@example.com>
2024-11-13 13:43:42 +0300

    Lab4

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function () {
+    window.addEventListener('load', function () {
+        const loadTime = (window.performance.now() / 1000).toFixed(2);
+        const footer = document.querySelector('footer');
+        if (footer) {
+            const statsDiv = document.createElement('div');
+            statsDiv.textContent = `Время загрузки страницы: ${loadTime} секунд`;
+            footer.appendChild(statsDiv);
+        }
+    });
+
+    window.addEventListener('DOMContentLoaded', function () {
+        const currentPath = document.location.pathname.split("/").pop();
+        const menuItems = document.querySelectorAll('nav ul li a');
+        menuItems.forEach((item) => {
+            const itemPath = item.getAttribute('href');
+            if (currentPath === itemPath) {
+                item.parentElement.classList.add('active');
+            }
+        });
+    });
+})();

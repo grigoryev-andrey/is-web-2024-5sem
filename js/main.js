850896a1aaba2c90628b43abfc01b4bed56b88b2
Your Name <you@example.com>
2024-11-13 14:11:24 +0300

    Lab4: fix active menu item in index.html

--- js/main.js
+++ js/main.js
@@ -10,15 +10,16 @@
     });
 
     window.addEventListener('DOMContentLoaded', function () {
-        let currentPath = document.location.pathname.split("/").pop();
-        if (currentPath === "" || currentPath === "/") {
-            currentPath = "index.html";
+        let currentPath = document.location.pathname;
+        if (currentPath === "/" || currentPath === "") {
+            currentPath = "/index.html";
         }
+        currentPath = currentPath.replace(/^\//, '');
         const menuItems = document.querySelectorAll('nav ul li a');
         menuItems.forEach((item) => {
             const itemPath = item.getAttribute('href');
-            if (currentPath === itemPath) {
-                item.parentElement.classList.add('active');
+            if (itemPath && currentPath.includes(itemPath)) {
+                item.classList.add('active');
             }
         });
     });

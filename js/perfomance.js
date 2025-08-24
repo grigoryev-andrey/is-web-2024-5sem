05f4115ddea8a565a9cc136c5e9a88c57239bdbd
Your Name <you@example.com>
2025-08-24 20:26:56 +0300

    delete unused scripts

--- js/perfomance.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function () {
-    const loadTime = (window.performance.now() / 1000).toFixed(2);
-    swal({
-        title: "Время загрузки",
-        text: `${loadTime} секунд`,
-        icon: "success",
-    });
-});
